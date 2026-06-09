a023c4a2324bd52195e3473d8addf0c4a3e4776e
Raja <gst.saha.raja@gmail.com>
2026-06-09 23:23:35 +0530

    MasterURI chnges patch

--- Arch/src/main/java/rs/demo/controller/MasterURIConstant.java
+++ Arch/src/main/java/rs/demo/controller/MasterURIConstant.java
@@ -4,5 +4,5 @@ public interface MasterURIConstant {
 public static final String GET_WELCOME = "/welcome";
 public static final String POST_WELCOME = "/postWelcome";
 public static final String PUT_WELCOME = "/putWelcome";
-
+public static final String PATCH_WELCOME = "/patchWelcome";
 }
