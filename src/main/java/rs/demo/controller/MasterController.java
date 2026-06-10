3a810091e5ecd5b780edf1c01b5addd658af18ec
Raja <gst.saha.raja@gmail.com>
2026-06-10 11:58:28 +0530

    From Poms

---
+++ src/main/java/rs/demo/controller/MasterController.java
@@ -0,0 +1,27 @@
+package rs.demo.controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import rs.demo.model.DemoReq;
+
+@RestController
+public class MasterController implements MasterURIConstant {
+    //Demo Purpose Get Mapping
+    @RequestMapping(value = MasterURIConstant.GET_WELCOME, method = RequestMethod.GET, produces = "application/json", headers = {
+			"Accept=application/json" })
+    public String helloPrint()
+     {
+        return "hello";
+     }
+     //Demo Purpose Post Mapping
+     @RequestMapping(value = MasterURIConstant.POST_WELCOME, method = RequestMethod.POST,produces = "application/json",headers = {
+        "Accept=application/json" })
+     public String namePrint(@RequestBody DemoReq demReq)
+     {
+        return "hello welcome  "+demReq.getName();
+     }
+
+
+     
+}
